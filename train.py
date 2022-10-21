69ab74092850ba0791fed6b26ef4dcb4edc05458
Herest <marco@amphorahealth.com>
2022-10-21 13:06:52 -0500

    Create train.py

---
+++ train.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 21 12:45:20 2022
+
+@author:  Marco A. Nava-Aguilar (marcoanavaaguilar@gmail.com)
+"""
+
+from GetUbigeo import *
+from dataReader import dataReader
+
+
