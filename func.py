9f7355fd3b54ea763bcf4d066bc5eb8fb8097fbd
Herest <marco@amphorahealth.com>
2022-10-21 12:35:31 -0500

    Update func.py

--- func.py
+++ func.py
@@ -8,22 +8,17 @@ def categoricalVia(tbl: pd.DataFrame() = None):
     labels={1:'CALLE', 2:'JIRON', 3:'AVENIDA', 4:'PASAJE', 5:'CARRETERA', 
             6:'MANZANA', 7:'LOTE', 8:'PARCELA', 9:'FUNDO', 10:'SIN_VIA'}
     try:
-        tbl['VIA'] = pd.cut(x = tbl['Tipo de vía'],
-                                  bins = [0,10,18.5,25,35,40,50,60,1000],
-                                  include_lowest=True, 
-                                  labels = labels.keys()
-                                 )
-        tbl['bmi_label'] = pd.cut(\
-                                  x = tbl['bmi'],
-                                  right=False,
-                                  bins = [0,10,18.5,25,35,40,50,60,1000],
-                                  include_lowest=True, 
-                                  labels = labels.values()
-                                 )
-
+        tbl['VIA'] = tbl['Tipo de vía'].apply(lambda x: labels[x])
+        tbl.drop('Tipo de vía',axis=1,inplace=True)
         return tbl
     
     except:
-        print('WARNING: bmi process failed')
+        print('WARNING: categorical transform process failed')
         raise
         return tbl
+    
+
+def downloadLoc(lat,long):
+    import googlemaps
+    gmaps = googlemaps.Client(key='AIzaSyC9WdDFLBJX0qVTQNxsQZCT_ERyxHB4s5c')
+    
