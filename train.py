9336ff5cc5c15f97f1295362d6c81698b236563a
Herest <marco@amphorahealth.com>
2022-10-21 17:06:21 -0500

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,13 @@ Created on Fri Oct 21 12:45:20 2022
 @author:  Marco A. Nava-Aguilar (marcoanavaaguilar@gmail.com)
 """
 
+import googlemaps
 from GetUbigeo import *
 from dataReader import dataReader
+
+dataReader=dataReader()
+df=dataReader.read_data('./Valorización Data Driven/dataset_tasacion_train_vf.xlsx')
+
+gmaps = googlemaps.Client(key=open('clave.txt').readline())
 
 
